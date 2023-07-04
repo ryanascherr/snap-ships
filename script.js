bec13cbf557c099fd97cb2745d4600554bb10ac6
ryanascherr <ryanascherr@gmail.com>
2023-07-04 01:40:17 -0400

    Added all Forge part cards. Changed image naming structure. Minor styling changes.

--- script.js
+++ script.js
@@ -88,6 +88,251 @@ const cards = [
         name: "Wasp KLAW Deep Recon",
         type: "Chassis",
         faction: "Komplex"
+    },
+    {
+        name: "BF Bulkhead",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Lance Booster",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Booster",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "XRE Extender",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "SC-19 Cockpit",
+        type: "Cockpit",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Cockpit",
+        type: "Cockpit",
+        faction: "Forge"
+    },
+    {
+        name: "XF-25 Cockpit",
+        type: "Cockpit",
+        faction: "Forge"
+    },
+    {
+        name: "Maneuvering Fins",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Fins",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Triple Tailfin",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "XR 135 Missile Rack",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "XR70 Missile Pod",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "H.I.V.E Missile Pod",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "SLS Solar",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "LS Eye",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "AVB Burst Drive",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Afterburner",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Thruster",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "Lance Thruster",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "SDU-14 Jump Engine",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "SV Engine",
+        type: "Thruster",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Cockpit",
+        type: "Cockpit",
+        faction: "Forge"
+    },
+    {
+        name: "XF-25 Cockpit",
+        type: "Cockpit",
+        faction: "Forge"
+    },
+    {
+        name: "Mark 48 Shipbreaker",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Medium Exhaust",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Nose",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "Small Exhaust",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "SCAR-4 Pulse Laser",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "MK16 Autocannon",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "TDP-8 Plasma Cannon",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "MX Massdriver Shotgun",
+        type: "Systems",
+        faction: "Forge"
+    },
+    {
+        name: "SC-19 Wings",
+        type: "Wing",
+        faction: "Forge"
+    },
+    {
+        name: "Falx Wings",
+        type: "Wing",
+        faction: "Forge"
+    },
+    {
+        name: "XF-25 Wings",
+        type: "Wing",
+        faction: "Forge"
+    },
+    {
+        name: "XJ-7 Wings",
+        type: "Wing",
+        faction: "Forge"
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
+    },
+    {
+        name: "",
+        type: "",
+        faction: ""
     }
 ]
 
@@ -95,6 +340,8 @@ showChassis();
 function showChassis() {
     $(cards).each(function() {
         let name = this.name.toLowerCase();
+        let type = this.type.toLowerCase();
+        let faction = this.faction.toLowerCase();
         name = name.split(" ");
         let length = name.length;
         let newName = "";
@@ -105,6 +352,11 @@ function showChassis() {
                 newName += name[i] + "-";
             }
         }
-        $(".card-container").append(`<img src="./card-images/ChassisCards/${newName}-front.png">`)
+        if (this.type == "Chassis") {
+            $(".card-container").append(`<img class="chassis" src="./card-images/${type}-${faction}-${newName}-front.png">`)
+        } else {
+            $(".card-container").append(`<img class="part" src="./card-images/${type}-${faction}-${newName}-front.png">`)
+        }
+        
     })
 }
